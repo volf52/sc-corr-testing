7a7e820cde09d4e11f8c3c93bdb48c1a3fea80e6
Arslan <rslnkrmt2552@gmail.com>
2020-08-18 17:11:13 +0300

    Add ops and corr_matrix methods (tentative to change)

--- pysc/ops.py
+++ pysc/ops.py
@@ -1,15 +1,40 @@
 from numba import vectorize as nvectorize
+import numpy as np
+import cupy as cp
 
+UNARY_OPS_SIGNATURE = "boolean(boolean)"
+BINARY_OPS_SIGNATURE = "boolean(boolean, boolean)"
 
-UNARY_OPS_SIGNATURE = 'boolean(boolean)'
-BINARY_OPS_SIGNATURE = 'boolean(boolean, boolean)'
-
-# Similar for or, not and xnot
+# These functions can be used directly on SCStream objects, thanks to the __array__ method
+# and are a liiiitle bit faster than the normal np logical methods
 
 @nvectorize(BINARY_OPS_SIGNATURE, nopython=True)
 def and_it(x, y):
-    return x and y
+    return x & y
+
+
+@nvectorize(BINARY_OPS_SIGNATURE, nopython=True)
+def or_it(x, y):
+    return x | y
+
 
 @nvectorize(UNARY_OPS_SIGNATURE, nopython=True)
 def not_it(x):
-    return not x
+    return ~x
+
+
+def corr_matrix(x, y):
+    a = and_it(x, y)
+    b = not_it(or_it(x, y))
+    c = and_it(not_it(x), y)
+    d = and_it(x, not_it(y))
+
+    return a,b,c,d
+
+
+corr_matrix_cuda = cp.ElementwiseKernel(
+    "bool x, bool y",
+    "bool a, bool b, bool c, bool d",
+    "a = x & y;b = x & ~y;c = ~x & y;d = ~(x | y)",
+    "find_corr_mat",
+)
